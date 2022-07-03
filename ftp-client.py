59adcf00746f96e09d2eba300dc8667398504a57
CzerPDX <93413807+CzerPDX@users.noreply.github.com>
2022-07-02 20:44:15 -0700

    Brooke (#20)
    * double test
    
    * updating getting started from readme
    
    * another update
    
    * test
    
    * merge is hard2
    
    * fds
    
    * readded deleted .gitignore file
    
    * added skeleton ui with naive logging implementation
    
    * added line about compiling to readme
    
    * changed default logging level from DEBUG to nothing
    
    * readded logging level after reading further. added some comments and referene material
    
    * messed around and added a simple login with exception handling to demonstrate how to use the ftplib functions
    
    * added logging to login example
    
    * added server response to log as well
    
    Co-authored-by: Brooke Czerwinski <czer@babbage.cs.pdx.edu>

--- ftp-client.py
+++ ftp-client.py
@@ -10,13 +10,18 @@ from ftplib import FTP
 def loginSecure(ftpAddr, usr):
     # Connect to ftp server
     ftp = FTP(ftpAddr)
-    
+
+    # log ftpAddr and username to log
+    logging.info("Attempted login: " + ftpAddr + ", " + usr)
+
     # log into ftp server and return the login response from the server
     try:
         resp = ftp.login(user=usr, passwd=input("enter pw: "))
+        logging.info(resp)
     # if an error occurs, return the error
     except Exception as err:
         resp = err
+        logging.error(err)
 
     return resp
 
