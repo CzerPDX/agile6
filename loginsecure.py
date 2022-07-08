0a5c60d00d717b186f77e233fe63077119d00016
Brooke Czerwinski <czer@babbage.cs.pdx.edu>
2022-07-07 23:02:58 -0700

    more refactoring. separated connect to ftp into its own file. create new ui that shows functions once logged into ftp

--- loginsecure.py
+++ loginsecure.py
@@ -4,14 +4,15 @@ from ftplib import FTP
 # Returns a tuple of (bool, string)
 # If success it will be: (True, welcomeMessage, ftp object)
 # If failure it will be: (False, Error message)
-def login-secure(ftpAddr, usr, ftp):
-    # log ftpAddr and username to log
-    logging.info("Attempted login: " + ftpAddr + ", " + usr)
+def loginSecure(ftp, usr):
+    resp = (True, "")
+    # log username from attempted login
+    logging.info("Attempted login: " + usr)
 
     # log into ftp server and return the login response from the server
     try:
         ftp.login(user=usr, passwd=input("enter pw: "))
-        resp = (True, ftp.getwelcome(), ftp)
+        resp = (True, ("\n" + ftp.getwelcome()), ftp)
         logging.info(resp)
     # if an error occurs, return the error
     except Exception as err:
