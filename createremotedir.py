9256dff8ec8ee57607ae6837dfe0c3062cfdc8c4
Jonas Persson <jpersson@pdx.edu>
2022-07-31 16:27:03 -0700

    Fixed logging typo in getremotedir module.

--- createremotedir.py
+++ createremotedir.py
@@ -11,12 +11,12 @@ def createDir(ftp):
         capture = ftp.mkd(newDir)
         ret = (True, (capture))
         now = datetime.now()
-        logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " COMMAND: CHANGE DIRECTORY ON FTP SERVER: Directory Created")
+        logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " COMMAND: CREATE DIRECTORY ON FTP SERVER: Directory Created")
     
     except Exception as err:
         ret = (False, err)
         now = datetime.now()
-        logging.error(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: CHANGE DIRECTORY ON FTP SERVER: " + str(err))
+        logging.error(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: CREATE DIRECTORY ON FTP SERVER: " + str(err))
 
     return ret
 
