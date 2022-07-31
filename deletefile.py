13bc796657fb04206f4d00119a1d788234fa2af9
Jonas Persson <jpersson@pdx.edu>
2022-07-31 16:14:36 -0700

    Added functionality for printing the input-and-errors.log. Added categories, descriptions, and timestamps to each command and error logging event. Fixed a menu typo and cleaned up the connections list. Added error logging to getfiles module functions.

--- deletefile.py
+++ deletefile.py
@@ -1,12 +1,15 @@
 import logging
 from ftplib import FTP
+from datetime import datetime
 
 def deleteFile(ftp, fileName):
 
     try:
         ftpResponse = ftp.delete(fileName);
-        logging.info(ftpResponse)
+        now = datetime.now()
+        logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " COMMAND: DELETE FILE ON FTP SERVER: " + ftpResponse)
     except Exception as err:
-        logging.error(err)
+        now = datetime.now()
+        logging.error(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: DELETE FILE ON FTP SERVER: " + str(err))
 
    
--- renamefile.py
+++ renamefile.py
@@ -1,6 +1,6 @@
 import logging
 from ftplib import FTP
-
+from datetime import datetime
 
 def renameFile(ftp):
 	
@@ -9,11 +9,13 @@ def renameFile(ftp):
 	try:
 		capture = ftp.rename(fromName, toName)
 		ret = (True, (capture))
-		logging.info("Files Renamed")
+		now = datetime.now()
+		logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " COMMAND: RENAME FILE ON FTP SERVER: Files Renamed.")
 
 	except Exception as err:
 		ret = (False, err)
-		logging.error(err)
+		now = datetime.now()
+		logging.error(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: RENAME FILE ON FTP SERVER: " + str(err))
 
 	return ret
 
