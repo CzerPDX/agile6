ad9c3a7832f256f999e51ec5f31165e736496966
John Pham <johpham@ada.cs.pdx.edu>
2022-08-02 14:56:55 -0700

    copyremotedir, it dont work yet

--- copyremotedir.py
+++ copyremotedir.py
@@ -1,13 +1,25 @@
 from ftplib import FTP
+import os  
 import logging
 
 
 def copyDir(ftp):
-    toCopy = input("Enter Name of Copy")
+
+    toCopy = input("Enter the Directory Name to Copy: ")
+    localPath = os.getcwd()
+    remotePath = ftp.pwd()
+
+    ftp.cwd(remotePath + toCopy)
+
+    newlocalPath = os.path.join(localPath, toCopy)
+    os.mkdir(newlocalPath)
+    os.chdir(newlocalPath)
+
     files = ftp.nlst()
+    print(files)
     for file in files:
         ftp.retrbinary("RETR "+file, open(file[1:], 'wb').write)
-    ftp.close
+
     
 
 
