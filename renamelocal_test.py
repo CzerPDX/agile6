0817ed14cd2842ff1776ed45373666360fa3c813
Brooke Czerwinski <czer@babbage.cs.pdx.edu>
2022-08-03 03:41:10 -0700

    standardized renamelocal and fixed its UI. Redid its testing to test more thoroughly

--- renamelocal_test.py
+++ renamelocal_test.py
@@ -7,13 +7,29 @@ from os.path import exists
 def test_rename_local_file():    
 
     #create a new file to be renamed then deleted
-    fp = open('test.txt', 'x')
+    ogFileName = "testname.txt"
+    newFileName = "newTest.txt"
+
+    # Check if the file exists already. Delete it if it does
+    if exists("./" + ogFileName) == True:
+        os.remove("./" + ogFileName)
+    assert (exists("./" + ogFileName) == False)
+
+    # Create a new version of the file and make sure it exists
+
+    fp = open("./" + ogFileName, 'x')
     fp.write("This is a test file, you should not be seeing this if the test does what it's suppose to")
     fp.close
+    assert (exists("./" + ogFileName) == True)
 
     #rename and delete if successful
-    assert(renamelocal.renameLocal('./test.txt','./newTest.txt') == True)
-    if(exists('./newTest.txt')):
-        os.remove('./newTest.txt')
-    else:
-        os.remove('./test.txt')
+    server_response = renamelocal.renameLocal(("./" + ogFileName),("./" + newFileName))
+    assert (server_response[0] == True)
+    assert (exists("./" + ogFileName) == False)
+    assert (exists("./" + newFileName) == True)
+
+    # Cleanup
+    if exists("./" + ogFileName) == True:
+        os.remove("./" + ogFileName)
+    if exists("./" + newFileName) == True:
+        os.remove("./" + newFileName)
