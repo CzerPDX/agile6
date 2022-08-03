2484b10b8cddc20a3c999944fe3b85e9f13df39f
JonasPerssonDevelops <88298443+JonasPerssonDevelops@users.noreply.github.com>
2022-08-03 15:04:30 -0700

    Added a test for the getfiles module get_multiple function to getfiles_test.py and added a test for the logtostring module process_log_file function to logtostring_test.py. (#74)

--- getfiles_test.py
+++ getfiles_test.py
@@ -23,6 +23,35 @@ def test_getfiles_get_single(monkeypatch):
 # getfiles get_single test:
     assert True == getfiles.get_single(ftp, "a.png", False)
     assert True == os.path.exists("a.png")
-
-# Close FTP connection
+    
+# Cleanup. Close connection and delete test files.
     ftp.quit()
+    os.remove("a.png")
+
+## connected and in a valid directory ## 
+def test_getfiles_get_single(monkeypatch):
+    # Establish FTP connection
+    ftpAddr = os.environ['FTPADDR']
+    connectionObj = connectftp.connectFTP(ftpAddr)
+    ftp = connectionObj[1]
+
+    # Get valid credentials
+    usr = os.environ['FTPUSR']
+    password = os.environ['FTPPASS']
+
+    ##need this to catch password stream before loginsecure
+    monkeypatch.setattr('builtins.input', lambda _: password)
+    loginsecure.loginSecure(ftp, usr)
+
+# getfiles get_multiple test:
+    test_list = [('a.png', False), ('tux.png', False)]
+    
+    assert True == getfiles.get_multiple(ftp, test_list)
+    assert True == os.path.exists("a.png")
+    assert True == os.path.exists("tux.png")
+
+# Cleanup. Close connection and delete test files.
+    ftp.quit()
+    os.remove("a.png")
+    os.remove("tux.png")
+    
