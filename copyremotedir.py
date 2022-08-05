96f4fae7b38859d649f8e73cc165d046d09e2474
John Pham <johpham@ada.cs.pdx.edu>
2022-08-05 12:22:24 -0700

    copyfunc with error stuff

--- copyremotedir.py
+++ copyremotedir.py
@@ -2,31 +2,112 @@ from ftplib import FTP
 import logging
 import getfiles
 import os
+from datetime import datetime
 
 def copyDir(ftp, toCopy):
-    localPath = os.getcwd()                     #Get local path
-    remotePath = ftp.pwd()
+
+    try:
+        localPath = os.getcwd()                     #Get local path
+        ret = (True, "")
+    
+    except Exception as err:
+        ret = (False, err)
+        logging.error(err)
+
+        now = datetime.now()
+        logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: " + str(err))
+
+    if(ret[0]):
+
+        try:
+            remotePath = ftp.pwd()
+            ret = (True, "")
+    
+        except Exception as err:
+            ret = (False, err)
+            logging.error(err)
+
+            now = datetime.now()
+            logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: " + str(err))
 
 
-    getfiles.get_directory(ftp, toCopy)          #Download Copy
+    if(ret[0]):
+        try:
+            getfiles.get_directory(ftp, toCopy)          #Download Copy
+            ret = (True, "")
+    
+        except Exception as err:
+            ret = (False, err)
+            logging.error(err)
 
+            now = datetime.now()
+            logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: " + str(err))
+        
+        
+    if(ret[0]):
+        try:
+            newlocalPath = os.path.join(localPath, toCopy)
+            ret = (True, "")
+    
+        except Exception as err:
+            ret = (False, err)
+            logging.error(err)
 
-    newlocalPath = os.path.join(localPath, toCopy)
-    os.chdir(newlocalPath)
+            now = datetime.now()
+            logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: " + str(err))
+
+    
+
+    if(ret[0]):
+        try:
+            capture = os.chdir(newlocalPath)
+            ret = (True, str(capture))
+    
+        except Exception as err:
+            ret = (False, err)
+            logging.error(err)
+
+            now = datetime.now()
+            logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: " + str(err))
+
+    if(ret[0]):
+        try:
+            newremotePath = remotePath + '/Copy of ' + toCopy
+            ftp.mkd(newremotePath)
+            ftp.cwd(newremotePath)
+
+            ret = (True, "")
+    
+        except Exception as err:
+            ret = (False, err)
+            logging.error(err)
+
+            now = datetime.now()
+            logging.info(now.strftime("%m/%d/%Y %H:%M:%S") + " ERROR: " + str(err))
+ 
+
 
     newremotePath = remotePath + '/Copy of ' + toCopy
     ftp.mkd(newremotePath)
     ftp.cwd(newremotePath)
-
+            
     files = os.listdir()
+    for file in files:
+        ftp.storbinary('STOR ' + file, open(file[0:], 'rb'))   #Upload Copy
+
 
     for file in files:
-        ftp.storbinary('STOR ' + file, open(file[0:], 'rb'))
-
-    for file in files:
-        toRemove = os.path.join(newlocalPath, file)
+        toRemove = os.path.join(newlocalPath, file)     #Delete temp copy files
         os.remove(toRemove)
+            
     os.rmdir(newlocalPath)
+
+
+
+
+
+        
+
 
 
 
